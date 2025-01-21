5f7c731187f376c8a1d6b88a8856061924401632
bo1ta <calinbercea007@gmail.com>
2025-01-21 23:23:50 +0200

    Setup initial app

---
+++ Audacounty/Audacounty/AudacountyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudacountyApp.swift
+//  Audacounty
+//
+//  Created by Solomon Alexandru on 21.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AudacountyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Audacounty/AudacountyTests/AudacountyTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudacountyTests.swift
+//  AudacountyTests
+//
+//  Created by Solomon Alexandru on 21.01.2025.
+//
+
+import Testing
+@testable import Audacounty
+
+struct AudacountyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
